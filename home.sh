7413e3cbed00aaccccc07e1168f2cbfdbf1e1f16
arnadas <142620137+arna-das@users.noreply.github.com>
2024-02-19 11:50:46 +0000

    Add files via upload

---
+++ home.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+clear
+echo "################################################################################"
+echo "#"                                                                
+echo "#                                 Not_arnadas"                         
+echo "#"                                                                      
+echo "#                         Copyright (C) 2023 - 2024"                 
+echo "#"                                                                      
+echo "#"                                                                    
+echo "################################################################################"
+echo ""
+echo ""
+echo "* [0] start the server"
+echo "* [1] update the ram"
+echo "* [2] download plugins"
+echo "* [3] update the repo"
+echo "* [4] dengeour zone"
+
+ 
+read -p " (0-4): " input
+
+case $input in
+    0)
+    ./on1.sh
+
+    ;;
+
+    1)
+    ./viabackword
+        ;;
+
+    2)
+    ./viabackword
+        ;;
+
+    3)
+    ./viabackword
+        ;;
+
+    4)
+    ./viabackword
+        ;;
+
+    5)
+    ./viabackword
+
+     
